9bca784562262ec7f982d202822906cfb61234ab
Dendaletche <jodenda@gmail.com>
2021-02-01 10:26:51 +0100

    Update sommaire.js

--- ressources/script/sommaire.js
+++ ressources/script/sommaire.js
@@ -11,13 +11,14 @@
 // déclarations des variables
 var tabItems = new Array();
 
-var baseURL = "niveau";
+var baseURL = "http://jodenda.free.fr/programmeNSI/";
+var URLs = ["../niveau_0.html","programmeNSIpremier_0.html","programmeNSITerm_0.html"];
 
 
 // initialisation des listes
-tabItems = ["enseignement de SNT en seconde",         
-			"enseignement de NSI en première",
-			"enseignement de NSI en terminale"];	
+tabItems = [	"enseignement de SNT en seconde"		,         
+		"enseignement de NSI en première"		,
+		"enseignement de NSI en terminale"		];	
 
 const nItems = tabItems.length; // ne change pas
 
@@ -31,5 +32,33 @@ function construireListeSommaire () {
 	for (var i=0; i < nItems; i++) {
 		listeSommaire.innerHTML += "<li><a href='"+baseURL+"_"+i+ ".html' target='_blank' >" +tabItems[i]+"</a></li>";
 		}
+	listeSommaire.innerHTML += 	"<li><a href='https://www.iut-rodez.fr/fr/les-formations/but-informatique/but-informatique-programme'"+
+					" target='_blank' >" +"programme du BUT d'informatique</a></li>";
+	
 	listeSommaire.innerHTML += "</ul>";
 	}
+/* autre méthode de constuction 
+	let i = tabItems.length, item, lien;
+	var listeSommaire = document.getElementById("listeSommaire");
+	let menu = document.createElement("ul");
+	for (var i=0; i < nItems; i++) {
+		item 		= document.createElement("li")	;
+		lien 		= document.createElement("a")	;
+		lien.href 	= baseURL+"_"+i+ ".html"	;
+		lien.innerHTML	= tabItems[i]			;
+		lien.alt 	= "lien vers " + tabItems[i]	;	
+		lien.title 	= "lien vers " + tabItems[i]	;
+		item.appendChild(lien);
+		menu.appendChild(item);
+	}
+	// items supplémentaires
+	item 		= document.createElement("li")	;
+	lien 		= document.createElement("a")	;
+	lien.href 	= "https://www.iut-rodez.fr/fr/les-formations/but-informatique/but-informatique-programme"	;
+	lien.innerHTML	= "programme du BUT d'informatique"		;
+	lien.alt 	= "lien vers programme du BUT d'informatique"	;	
+	lien.title 	= "lien vers programme du BUT d'informatique"	;
+	item.appendChild(lien);
+	menu.appendChild(item);
+	listeSommaire.appendChild(menu);
+*/
