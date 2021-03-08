7bef6d5d308e619f58f9b8f0496934dbff2d31c8
Dendaletche <jodenda@gmail.com>
2021-03-08 13:42:38 +0100

    Create listesIdeesGrandoral.js

---
+++ ressources/script/listesIdeesGrandoral.js
@@ -0,0 +1,54 @@
+/***************************************************************************
+**                                                                        **
+**      listes des thématiques et idées de questions associées            **
+**                    pour le grand oral du bac                           **
+**                                                                        **
+****************************************************************************/
+const themes = [
+  "L’histoire de l’informatique",
+  "Langages et programmation",
+  "Données structurées et structures de données",
+  "Bases de données",
+  "Architectures matérielles, systèmes d’exploitation et réseaux",
+  "Interfaces Hommes-Machines (IHM)"];
+const questions = [
+  [
+    "Femmes et numérique : quelle histoire ? quel avenir ?",
+    "Ada Lovelace, pionnière du langage informatique",
+    "Les calculatrices de la NASA (étaient des femmes !)",
+    "Alan Turing, et l’informatique fut",
+    "Quelle est la différence entre le web 1.0 et le web 2.0 ?",
+    "La numérisation des prévisions météo"
+  ],[
+    "P = NP, un problème à un million de dollars ?",
+    "Tours de Hanoï : plus qu’un jeu d’enfants ?",
+    "Les fractales : informatique et mathématiques imitent-elles la nature ?",
+    "De la récurrence à la récursivité","Les bugs : bête noire des développeurs ?", 
+    "Comment rendre l’informatique plus sûre ?"
+  ],[
+    "L’informatisation des métros : progrès ou outil de surveillance ?",
+    "Musique et informatique : une alliance possible de l’art et de la science ?"
+  ],[
+    "Comment créer une machine intelligente ?",
+    "Comment lutter contre les biais algorithmiques ?",
+    "Quels sont les enjeux de la reconnaissance faciale (notamment éthiques) ?	",
+    "Quels sont les enjeux de l’intelligence artificielle ?",
+    "Transformation d’images : Deep Fakes, une arme de désinformation massive ? La fin de la preuve par l’image ?",
+    "Qu’apporte la récursivité dans un algorithme ?",
+    "Quel est l’impact de la complexité d’un algorithme sur son efficacité ?"
+  ],[
+    "Données personnelles : la vie privée en voie d’extinction ?",
+    "Comment optimiser les données ?"
+  ],[
+    "L’ordinateur quantique : nouvelle révolution informatique ?",
+    "La course à l’infiniment petit : jusqu’où ?",
+    "Peut-on vraiment sécuriser les communications ?",
+    "Quelle est l’utilité des protocoles pour l’internet ?",
+    "Cyberguerre : la 3ème guerre mondiale ?"
+  ],[
+    "Smart cities, smart control ?",
+    "La réalité virtuelle : un nouveau monde ?",
+    "La voiture autonome, quels enjeux ?"
+  ],[
+    "Comment protéger les données numériques sur les réseaux sociaux ?",
+    "","","",""]];
